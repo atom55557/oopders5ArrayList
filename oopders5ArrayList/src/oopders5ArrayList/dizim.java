b7979ac534018161db37f417bbedf5d99e51197b
serha <serha@LAPTOP-HMCTAEDF>
2022-10-31 09:30:00 +0300

    arraylist1

--- oopders5ArrayList/src/oopders5ArrayList/dizim.java
+++ oopders5ArrayList/src/oopders5ArrayList/dizim.java
@@ -1,7 +1,8 @@
 package oopders5ArrayList;
-import java.util.ArrayList;
-public class ArrayList {
 
+import java.util.ArrayList;
+
+public class dizim {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<String> item=new ArrayList<String>();
@@ -13,8 +14,11 @@ public class ArrayList {
 		}
 		item.remove("red");
 		for(String renk:item) {
-			System.out.print(" "+renk);
+			System.out.print("  "+renk);
 		}
+		System.out.print(item.get(1));
+		
+		System.out.print(item.contains("red")?" evet var.":" yok.");
 
 	}
 
